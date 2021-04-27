5dae0719a804b1bcd873f561e994c62264797543
Edu-Sam <edungugi20@gmail.com>
2021-04-27 13:02:29 +0300

    Second Commit

---
+++ src/main/java/com/express/users/service/UsersService.java
@@ -0,0 +1,27 @@
+package com.express.users.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.express.users.entity.Users;
+import com.express.users.repository.UsersRepository;
+
+@Component
+public class UsersService {
+	
+	private UsersRepository usersRepository;
+
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public List<Users> getUsers() {
+        return usersRepository.findAll();
+    }
+    
+    public Users saveUser(Users users) {
+    	return usersRepository.save(users);
+    }
+
+}
